9c4b5b0490147dbbb5caba08e750c22f6af9b5b0
RichaLimbachiya <richalimbachiya2004@gmail.com>
2025-05-13 12:55:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+// Get the current URL
+const currentURL = window.location.href;
+
+// Calculate the length of the URL
+const urlLength = currentURL.length;
+
+// Display the result
+alert("The length of the URL is: " + urlLength);
